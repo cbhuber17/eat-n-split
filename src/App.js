c8c73f17d3cd2caeb7e23df871f0b2ff786e4c0e
Colin Huber <cbhuber@gmail.com>
2023-06-24 16:32:48 -0600

    feat: Form split bill.

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ export default function App() {
         <FormAddFriend />
         <Button>Add Friend</Button>
       </div>
+
+      <FormSplitBill />
     </div>
   );
 }
@@ -80,6 +82,33 @@ function FormAddFriend() {
       <input type="text" />
 
       <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split the bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🕴️ Your expenses</label>
+      <input type="text" />
+
+      <label>X's expense</label>
+      <input type="text" disabled />
+
+      <label>
+        🤑 Who is paying the bill?
+        <select>
+          <option value="user">You</option>
+          <option value="friend">X</option>
+        </select>
+      </label>
+
+      <Button>Split bill</Button>
     </form>
   );
 }
