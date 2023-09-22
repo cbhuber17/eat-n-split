ef4026952410cae88a6029de8a3591b72af677b8
Colin Huber <cbhuber@gmail.com>
2023-09-22 17:52:08 -0600

    feat: make page responsive.  Remove initial friends.

--- src/App.js
+++ src/App.js
@@ -4,29 +4,8 @@ import { FriendsList } from "./components/FriendsList";
 import { FormAddFriend } from "./components/FormAddFriend";
 import { FormSplitBill } from "./components/FormSplitBill";
 
-const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
-];
-
 export default function App() {
-  const [friends, setFriends] = useState(initialFriends);
+  const [friends, setFriends] = useState([]);
   const [showAddFriend, setShowAddFriend] = useState(false);
   const [selectedFriend, setSelectedFriend] = useState(null);
 
